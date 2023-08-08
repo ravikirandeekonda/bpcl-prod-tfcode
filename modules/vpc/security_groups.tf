610ec6294232ded2e7a94d57f6c67b4119496dda
ravikirandekonda-c4c <deekonda.ravikiran@cloud4c.com>
2023-08-08 18:30:11 +0530

    modified sg

--- modules/vpc/security_groups.tf
+++ modules/vpc/security_groups.tf
@@ -6,13 +6,13 @@ resource "aws_security_group" "app1sg" {
     from_port = 22
     to_port = 22
     protocol = "TCP"
-    cidr_block = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress{
     from_port = 0
     to_port = 0
     protocol = "-1"
-    cidr_block = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
