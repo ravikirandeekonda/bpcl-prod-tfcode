66dc40dfd988e0df4a24edaa5179c8ae605e56ff
ravikirandekonda-c4c <deekonda.ravikiran@cloud4c.com>
2023-08-08 20:45:46 +0530

    added output file in ec2

--- modules/ec2/ec2.tf
+++ modules/ec2/ec2.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "app1" {
   ami = "ami-0df7a207adb9748c7"
   subnet_id = var.pubsub1aid
   instance_type = "t2.micro"
-  keypair = "ravikiran-keypair"
+  key_name = "ravikiran-keypair"
   security_groups = [var.security_groups]
 
   root_block_device {
