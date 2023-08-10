0411fd9452229fc89ed92323c7f09a379b1e82e0
ravikirandekonda-c4c <deekonda.ravikiran@cloud4c.com>
2023-08-10 22:28:00 +0530

    modified

--- dev/main.tf
+++ dev/main.tf
@@ -10,7 +10,7 @@ module "ec2mod" {
   depends_on     = [module.vpcmod]
   source         = "../modules/ec2"
   publicsubent1a = module.vpcmod.publicsubnet1a
-  publicsubnet1b = module.vpcmod.publicsubnet1b
+  publicsubnet1b = module.vpcmod.publicsubent1b
   jenkinsg       = module.vpcmod.jenkinsg
   appsg          = module.vpcmod.appsg
 }
